223fc52fb18765a26bf1da9cce3cb8f2f299d6e9
resatraz <resatraz@gmail.com>
2026-08-05 22:53:37 +0300

    cd C:\Users\Nisa\Documents\FlutterProjeler\servisnoktam_veli git init git remote add origin https://github.com/noktaturizm63/servisnoktam_veli.git git add . git commit -m "Harita başlangıç noktası Şanlıurfa yapıldı, telif hakkı eklendi" git branch -M main git push -u origin main

--- src/pages/LiveTracking.jsx
+++ src/pages/LiveTracking.jsx
@@ -91,7 +91,7 @@ const LiveTracking = () => {
 
       {/* Map */}
       <div className="bg-white rounded-lg shadow-sm overflow-hidden" style={{ height: '600px' }}>
-        <MapContainer center={[39.9334, 32.8597]} zoom={10} style={{ height: '100%', width: '100%' }}>
+        <MapContainer center={[37.1674, 38.7955]} zoom={12} style={{ height: '100%', width: '100%' }}>
           <TileLayer
             attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
